e348aa53d0a4a7d1ab63c45740d8f8be3b3b2eb1
Sheikh Abdullah <123625439+SheikhAbdullahprogrammer@users.noreply.github.com>
2024-02-01 17:15:43 -0800

    Add files via upload

---
+++ Music Player/src/data.js
@@ -0,0 +1,79 @@
+ let songs = [
+
+    {
+
+        name: "song 1",
+        path:  "./assests/musics/Song 1.mp3" , 
+        artist: "artist 1",
+        cover: "./assests/images/cover1.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 2",
+        path:  "./assests/musics/Song 2.mp3" , 
+        artist: "artist 2",
+        cover: "./assests/images/cover2.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 3",
+        path:  "./assests/musics/Song 3.mp3" , 
+        artist: "artist 3",
+        cover: "./assests/images/cover3.png.jpg"
+    }
+ , 
+    
+    {
+
+        name: "song 4",
+        path:  "./assests/musics/Song 4.mp3" , 
+        artist: "artist 4",
+        cover: "./assests/images/cover4.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 5",
+        path:  "./assests/musics/Song 5.mp3" , 
+        artist: "artist 5",
+        cover: "./assests/images/cover3.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 6",
+        path:  "./assests/musics/Song 6.mp3" , 
+        artist: "artist 6",
+        cover: "./assests/images/cover6.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 7",
+        path:  "./assests/musics/Song 7.mp3" , 
+        artist: "artist 7",
+        cover: "./assests/images/cover7.png.jpg"
+    }
+
+    ,
+
+    {
+
+        name: "song 8",
+        path:  "./assests/musics/Song 8.mp3" , 
+        artist: "artist 8",
+        cover: "./assests/images/cover8.png.jpg"
+    }
+];
